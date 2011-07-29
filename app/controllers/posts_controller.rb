c65307a1542900c1ebb054a97e38be2d568d93ab
Adman65 <Adman1965@gmail.com>
2011-07-29 11:56:59 +0300

    Use responders

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,24 @@
 class PostsController < ApplicationController
+  respond_to :json, :html
+
   def index
     @post = Post.with_picture.first
+
+    respond_with @post do |wants|
+      wants.html
+      wants.json { render :json => @post.all }
+    end
   end
 
   def pictures
     @posts = Post.with_picture.all
+
+    respond_with @posts
   end
 
   def music
     @posts = Post.with_track.all
+
+    respond_with @posts
   end
 end
