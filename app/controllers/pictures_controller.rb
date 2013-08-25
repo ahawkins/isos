36c51188a9e350352581f9e227655b3253f202a0
ahawkins <me@broadcastingadam.com>
2013-08-25 19:01:25 +0200

    Use id for an etag

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,18 +1,18 @@
 class PicturesController < ApplicationController
   def wall
     picture = Post.picture params[:picture_id]
-    content = picture.image.wall.read
 
-    if stale? etag: content, public: true
+    if stale? etag: picture.id, public: true
+      content = picture.image.wall.read
       send_data content, type: picture.image.file.content_type, disposition: "inline"
     end
   end
 
   def gallery
     picture = Post.picture params[:picture_id]
-    content = picture.image.gallery.read
 
-    if stale? etag: content, public: true
+    if stale? etag: picture.id, public: true
+      content = picture.image.gallery.read
       send_data content, type: picture.image.file.content_type, disposition: "inline"
     end
   end
