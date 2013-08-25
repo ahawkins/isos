8214ab3ac62613308dc4882f6759f2fffb95e3f6
ahawkins <me@broadcastingadam.com>
2013-08-25 18:34:05 +0200

    Use caching

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,12 +2,18 @@ class PicturesController < ApplicationController
   def wall
     picture = Post.picture params[:picture_id]
     content = picture.image.wall.read
-    send_data content, type: picture.image.file.content_type, disposition: "inline"
+
+    if stale? etag: content, public: true
+      send_data content, type: picture.image.file.content_type, disposition: "inline"
+    end
   end
 
   def gallery
     picture = Post.picture params[:picture_id]
     content = picture.image.gallery.read
-    send_data content, type: picture.image.file.content_type, disposition: "inline"
+
+    if stale? etag: content, public: true
+      send_data content, type: picture.image.file.content_type, disposition: "inline"
+    end
   end
 end
