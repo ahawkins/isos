aa970b567b0017fd2c5599a4a8e7890c2437a638
ahawkins <adam@hawkins.io>
2015-08-25 01:21:49 +0530

    Use EXIF dates only

--- generate.rb
+++ generate.rb
@@ -3,15 +3,33 @@
 require 'json'
 require 'time'
 require 'erb'
+require 'open3'
 
 Entry = Struct.new(:image, :date, :location)
 Section = Struct.new(:date, :entries)
+
+ExifError = Class.new StandardError
 
 @entries = JSON.load($stdin).map do |data|
   Entry.new.tap do |entry|
     entry.image = "images/#{data.fetch('image')}"
     entry.location = data.fetch('location')
-    entry.date = Time.parse(data.fetch('date'))
+
+    stdin, stdout, stderr, exif = Open3.popen3 %Q{exif -t "Date and Time" --machine-readable "images/#{data.fetch('image')}"}
+
+    if exif.value.success?
+      raw = stdout.read
+      parts = raw.split ' '
+      year = parts[0].gsub(':', '-')
+      time = parts[1]
+
+      stdout.close
+      stderr.close
+
+      entry.date = Time.parse "#{year}T#{time}Z"
+    else
+      fail ExifError, "#{data.fetch('image')} - #{stderr.read}"
+    end
   end
 end.sort do |e1, e2|
   e2.date <=> e1.date
